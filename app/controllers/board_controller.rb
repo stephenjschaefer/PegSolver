832c329dbdde59ce27fb1c47851cf7d0433d9d21
Steve Schaefer <stephen.schaefer@me.com>
2016-01-07 02:35:58 -0500

    Removed alternate methods of drawing the board. Added additional css to center the heading and add some left padding to the board.

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -8,7 +8,7 @@ class BoardController < ApplicationController
     @row3 = "&nbsp;&nbsp;/#{board.decode(@state[3])} #{board.decode(@state[4])} #{board.decode(@state[5])}\\&nbsp;&nbsp;"
     @row4 = "&nbsp;/#{board.decode(@state[6])} #{board.decode(@state[7])} #{board.decode(@state[8])} #{board.decode(@state[9])}\\&nbsp;"
     @row5 = "/#{board.decode(@state[10])} #{board.decode(@state[11])} #{board.decode(@state[12])} #{board.decode(@state[13])} #{board.decode(@state[14])}\\"
-    @bottom = '‾‾‾‾‾‾‾‾‾‾‾‾'
+    @bottom = '‾‾‾‾‾‾‾‾‾‾‾'
     @rows = [@top, @row1, @row2, @row3, @row4, @row5, @bottom]
   end
 end
